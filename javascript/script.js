ff66e0e2ae43e31b40fb0c7a725a90ebad5d2bad
Dyego Castro <dcastrocerqueira33@gmail.com>
2023-02-26 22:09:26 -0300

    Atualizado

--- javascript/script.js
+++ javascript/script.js
@@ -1,16 +1,19 @@
-$(document).ready(() => {
-    $('#informacoesFinans').hide();
-    $('#informacoesUrna').hide();
-});
+function aparacerDescricao(app){
+    if(app == 'calculadora'){
+        document.querySelector("#calc").style.transition = "2s"
+        document.querySelector("#calc").style.boxShadow = "2px 2px 2px"
+        document.querySelector("#calc").style.backgroundColor = "#f8f8ff"
+        document.querySelector("#informacoesCalc").innerHTML = "Calculadora personalizada com animações nas esferas. Dois botões um muda para modo Dark e o outro para mudar de calculadora. <br> Tecnologias usadas: HTML, CSS e Javascript."
+    }else if(app == 'finans'){
+        document.querySelector("#finans").style.transition = "2s"
+        document.querySelector("#finans").style.boxShadow = "2px 2px 2px"
+        document.querySelector("#finans").style.backgroundColor = "#f8f8ff"
+        document.querySelector("#informacoesFinans").innerHTML = "Site Finans simples com o objetivo de estudos para parte de front end com alguns botões, input e responsivo. <br/> Tecnologias usadas: HTML, CSS e Bootstrap."
+    }else if(app == 'urna'){
+        document.querySelector("#urna").style.transition = "2s"
+        document.querySelector("#urna").style.boxShadow = "2px 2px 2px"
+        document.querySelector("#urna").style.backgroundColor = "#f8f8ff"
+        document.querySelector("#informacoesUrna").innerHTML = "Simulador de urna eletrônica com botões para digitar os números dos candidato e confirma, números dos canditados encontra-se no botão código. <br> Tecnologias usadas: HTML, CSS e Javascript."
+    }
+}
 
-$(document).ready(() => {
-    $('#finans').mouseenter(() => {
-        $('#informacoesFinans').fadeIn(1000).html('Simulador site Finans. <br/> Tecnologias usadas: HTML, CSS e Bootstrap.');
-    });
-});
-
-$(document).ready(() => {
-    $('#urna').mouseenter(() => {
-        $('#informacoesUrna').fadeIn(1000).html('Simulador de urna eletrônica. <br> Tecnologias usadas: HTML, CSS e Javascript.');
-    });
-});
