a387dd03cd8c9700ab3c235756a270ec6e99a5de
Dyego Castro <dcastrocerqueira33@gmail.com>
2023-10-19 04:36:00 -0300

    'Up'

--- javascript/script.js
+++ javascript/script.js
@@ -18,12 +18,11 @@ function aparacerDescricao(app){
         document.querySelector("#urna").style.height = "31em"
         document.querySelector("#informacoesUrna").innerHTML = "Simulador de urna eletrônica com botões para digitar os números dos candidato e confirma, números dos canditados encontra-se no botão código. <br> Tecnologias usadas: HTML, CSS e JavaScript."
     }else if(app == 'food'){
-        document.querySelector("#food").style.transition = "2s"
-        
+        document.querySelector("#food").style.transition = "2s"     
         document.querySelector("#food").style.boxShadow = "2px 2px 2px"
         document.querySelector("#food").style.backgroundColor = "#f8f8ff"
         document.querySelector("#food").style.height = "31em"
-        document.querySelector("#informacoesFood").innerHTML = "E-commerce de hamburguers, refrigerantes e sobremesas, responsivo para celulares, no momento estou trabalhando nesse projeto ele náo está finalizado. <br> Tecnologias usadas: HTML, CSS, JavaScript e React."
+        document.querySelector("#informacoesFood").innerHTML = "E-commerce de hamburguers, responsivo para celulares, no momento estou trabalhando nesse projeto ele náo está finalizado. <br> Tecnologias usadas: HTML, CSS, JavaScript e React."
     }
 }
 
